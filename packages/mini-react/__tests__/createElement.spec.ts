9f63869cb542c6b59351a940be63a3b766abad25
wangkaiwd <1162486176@qq.com>
2022-09-17 19:47:29 +0800

    feat: create element without jsx

---
+++ packages/mini-react/__tests__/createElement.spec.ts
@@ -0,0 +1,25 @@
+import { createElement } from '../core/react';
+
+describe('createElement', () => {
+  it('should create virtual node', () => {
+    const node = createElement(
+      'h1',
+      { className: 'a' },
+      createElement('span', {}, 'a'),
+      createElement('span', {}, 'b')
+    );
+    const node1 = {
+      type: 'h1',
+      props: {
+        key: undefined,
+        ref: undefined,
+        className: 'a',
+        children: [
+          { type: 'span', props: { key: undefined, ref: undefined, children: 'a' } },
+          { type: 'span', props: { key: undefined, ref: undefined, children: 'b' } }
+        ]
+      }
+    };
+    expect(node).toEqual(node1);
+  });
+});
--- packages/mini-react/core/react.ts
+++ packages/mini-react/core/react.ts
@@ -1,12 +1,18 @@
-// createElement('h1',{className: 'a'}, [createElement('span',{},'a'),createElement('span',{},'b')])
+const normalizeChildren = (children: any[]) => {
+  if (children.length === 1 && typeof children[0] === 'string') {
+    return children[0];
+  }
+  return children;
+};
 export const createElement = (type: any, props: Record<any, any>, ...children: any[]) => {
   const { ref, key, ...restProps } = props;
+  const normalizedChildren = normalizeChildren(children);
   return {
     ref,
     key,
     props: {
       ...restProps,
-      children
+      children: normalizedChildren
     },
     type,
   };
