e6f98309df7227082441773caddb32385cc8e22a
wangkaiwd <1162486176@qq.com>
2022-10-06 16:57:07 +0800

    feat: implement some basic react hooks

--- packages/mini-react/core/hooks.ts
+++ packages/mini-react/core/hooks.ts
@@ -10,23 +10,59 @@ export const hookStore: HookStore = {
   hookStates: []
 };
 export const useState = (initialState?: any) => {
+  return useReducer(null, initialState);
+};
+
+export const useReducer = (reducer: ((state: any, action: any) => any) | null, initialState: any) => {
   const { hookIndex: index, hookStates } = hookStore;
   const state = hookStates[index] ?? initialState;
   hookStore.hookIndex++;
-  const setState = (newState: any) => {
+  const dispatch = (actionOrNewState: any) => {
+    const newState = reducer ? reducer(state, actionOrNewState) : actionOrNewState;
     if (newState === state) return;
     hookStates[index] = newState;
     // each update from component root? diff whole component tree?
     // yes, React doesn't know where need to update
     hookStore.scheduleUpdate?.();
   };
-  return [state, setState];
+  return [state, dispatch];
 };
 
-export const useMemo = (fn: () => any, deps: any[]) => {
+function isSameDeps (lastDeps: any, currentDeps: any[]) {
+  if (lastDeps.length !== currentDeps.length) {return false;}
+  for (let i = 0; i < currentDeps.length; i++) {
+    const currentDep = currentDeps[i];
+    const lastDep = lastDeps[i];
+    if (lastDep !== currentDep) {
+      return false;
+    }
+  }
+  return true;
+}
 
+export const useMemo = (fn: () => any, deps: any[]) => {
+  const { hookStates, hookIndex } = hookStore;
+  hookStore.hookIndex++;
+  if (hookStates[hookIndex]) {
+    const [lastMemo, lastDeps] = hookStates[hookIndex];
+    if (isSameDeps(lastDeps, deps)) {
+      return lastMemo;
+    }
+  }
+  const memo = fn();
+  hookStates[hookIndex] = [memo, deps];
+  return memo;
 };
 
 export const useCallback = (fn: () => any, deps: any[]) => {
-
+  const { hookStates, hookIndex } = hookStore;
+  hookStore.hookIndex++;
+  if (hookStates[hookIndex]) {
+    const [lastCallback, lastDeps] = hookStates[hookIndex];
+    if (isSameDeps(lastDeps, deps)) {
+      return lastCallback;
+    }
+  }
+  hookStates[hookIndex] = [fn, deps];
+  return fn;
 };
--- packages/playground/src/Hooks.tsx
+++ packages/playground/src/Hooks.tsx
@@ -1,12 +1,26 @@
-import { createElement, useState } from '@sppk/mini-react';
+import { createElement, useCallback, useMemo, useState } from '@sppk/mini-react';
 
 const Hooks = () => {
   const [count, setCount] = useState(0);
-  const onClick = () => {
+  const [age, setAge] = useState(0);
+  const onClickCount = () => {
     console.log('click');
     setCount(count + 1);
   };
-  return createElement('div', { onClick }, count);
+  const double = useMemo(() => {
+    console.log('double');
+    return count * 2;
+  }, [count]);
+
+  const onClickAge = useCallback(() => {
+    setAge(age + 1);
+  }, []);
+
+  return createElement(
+    'div', {},
+    createElement('div', { onClick: onClickCount }, `${count}:${double}`),
+    createElement('div', { onClick: onClickAge }, age)
+  );
 };
 
 export default Hooks;
